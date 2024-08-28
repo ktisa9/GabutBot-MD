5a4a1207129d4cf6ec44b6695acd2aea337ac285
ktisa9 <gwarojustus20@gmail.com>
2024-08-28 21:42:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ global.namabot = 'ɢᴀʙᴜᴛʙᴏᴛ-ʀᴇʙᴏʀɴ'
 global.namaowner = 'ShinySebastian'
 
 //—————「 Setting Owner 」—————//
-global.owner = '628116646665'
-global.nomorlu = '6281932309239'
-global.ownernomer = ["628116646665"]
-global.premium = ['628116646665']
+global.owner = '254748840328'
+global.nomorlu = '254748840328'
+global.ownernomer = ["254748840328"]
+global.premium = ['254748840328']
 
 //—————「 Set Wm 」—————//
 global.packname = 'GabutBot-Reborn'
